1aeba1f2096580ca74681b5df3dd1c001fdca780
SHKlarenbeek <34334@ma-web.nl>
2022-05-12 10:04:42 +0200

    Dialogue

---
+++ C# scripts/DialogueManager/DialogueManager/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+
+    private Queue<string> sentences;
+
+
+    void start ()
+    {
+        sentences = new Queue<string>();
+
+    }
+
+
+}
---
+++ C# scripts/DialogueTrigger/DialogueTrigger/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueTrigger : MonoBehaviour
+{
+
+
+    public Dialogue dialogue;
+}
