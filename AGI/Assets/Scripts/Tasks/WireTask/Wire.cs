e4d2038b06940cfa758a6443db2a6293f9bdc66f
SHKlarenbeek <34334@ma-web.nl>
2022-06-16 11:32:43 +0200

    Power task

--- AGI/Assets/Scripts/Tasks/WireTask/Wire.cs
+++ AGI/Assets/Scripts/Tasks/WireTask/Wire.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Wire : MonoBehaviour
 {
+    public SpriteRenderer WireMoving;
+    Vector3 startPoint;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,10 +15,18 @@ public class Wire : MonoBehaviour
     {
         //mouse position to world point
 
-        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         newPosition.z = 0;
 
         //update wire
         transform.position = newPosition;
+
+        // update direction
+        Vector3 direction = newPosition - startPoint;
+        transform.right = direction * transform.lossyScale.x;
+
+        // update scale
+        float dist = Vector2.Distance(startPoint, newPosition);
+        WireMoving.size = new Vector2(dist, WireMoving.size.y);
     }
 }
